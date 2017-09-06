e79c877369b58928db1b824eba2e70ef7451aede
Edwin Ajahuanca <edwinajahuancacallisaya@gmail.com>
2017-09-06 00:10:00 -0400

    Controlador Municipio y sus metodos CRUD

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
+use App\Http\Requests\MunicipioRequest;
 use App\Http\Requests;
+use App\Provincia;
 use App\Municipio;
 use Carbon\Carbon;
+use Session;
 
 class MunicipioController extends Controller
 {
@@ -17,76 +21,83 @@ class MunicipioController extends Controller
 
     public function index()
     {
-        //
+        $$municipio = $municipio::orderBy('created_at','DESC')->get();
+        return view('admin.$municipio.index')->with('$municipio', $$municipio);
     }
 
     public function getMunicipios(Request $request)
     {
         if($request->ajax())
         {
-            $rpta = Municipio::select('id','mun_nombre')->where('mun_estado',1)->where('provincia_id','=', $request->provinciaID)->orderBy('mun_nombre','ASC')->get();
+            $rpta = $municipio::select('id','mun_nombre')->where('mun_estado',1)->where('provincia_id','=', $request->provinciaID)->orderBy('mun_nombre','ASC')->get();
             return response()->json($rpta);
         }
     }
 
     public function create()
     {
-        //
+        $provincia = Provincia::where('prov_estado','=',1)->orderBy('prov_nombre','ASC')->lists('prov_nombre','id');
+        return view('admin.unidad.create')->with('provincia', $provincia);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(MunicipioRequest $request)
     {
-        //
+        $title = '';
+        $msg = '';
+        $estado = false;
+        try
+        {
+            $municipio = new Municipio($request->all());
+            $municipio->user_registra = Auth::user()->id;
+            $municipio->user_actualiza = Auth::user()->id;
+            $municipio->save();
+            $title = 'Registro de Municipio';
+            $msg = 'Se realizo el registro de manera satisfactoria';
+            $estado = true;
+        }
+        catch(\Exception $ex)
+        {
+            $title = 'Error en Registro';
+            $msg = 'No se puede realizar el registro';
+        }
+        Session::put('estado', $estado);
+        Session::put('title', $title);
+        Session::put('msg', $msg);
+        return redirect()->route('municipio.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        $provincia = Provincia::where('prov_estado','=',1)->orderBy('prov_nombre','ASC')->lists('prov_nombre','id');
+        $municipio = Municipio::find($id);
+        return view('admin.municipio.edit')
+                ->with('municipio', $municipio)
+                ->with('provincia', $provincia);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(MunicipioRequest $request, $id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $estado = false;
+        $title = '';
+        $msg = '';
+        try
+        {
+            $municipio = Municipio::find($id);
+            $municipio->fill($request->all());
+            $municipio->user_actualiza = Auth::user()->id;
+            $municipio->update();
+            $estado = true;
+            $title = 'Actualización de Municipio';
+            $msg = 'Se realizo la actualización de manera satisfactoria.';
+        }
+        catch(\Exception $ex)
+        {
+            $title = 'Error en Actualización';
+            $msg = 'No se puede actualizar el Municipio.';
+        }
+        Session::put('estado', $estado);
+        Session::put('title', $title);
+        Session::put('msg', $msg);
+        return redirect()->route('municipio.index');
     }
 }
