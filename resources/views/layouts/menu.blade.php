a1e436c3f913e52fde4ca52e6d6813fda7220ef0
Edwin Ajahuanca <edwinajahuancacallisaya@gmail.com>
2017-09-10 11:07:18 -0400

    Adicionando nuevos link en el menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -28,6 +28,36 @@
                     <i class="fa fa-list"></i> <span>Proyectos a Evaluar</span>
                 </a>
             </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-gear"></i> <span>Parametrizaciones</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li>
+                        <a href="{{ url('/entidad') }}">
+                            <i class="fa fa-list"></i> <span>Entidades (UE)</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ url('/unidad') }}">
+                            <i class="fa fa-list"></i> <span>Unidades</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ url('/provincia') }}">
+                            <i class="fa fa-list"></i> <span>Provincias</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ url('/municipio') }}">
+                            <i class="fa fa-list"></i> <span>Municipios</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
         </ul>
     </section>
     <!-- /.sidebar -->
